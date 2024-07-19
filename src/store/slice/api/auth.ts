6a4eb57df362ca51b41a8fd1d6e466c45ad812dd
PragramAllDay <ibrhaimimtiaz59@gmail.com>
2024-07-19 16:03:34 +0500

    removed referrer policy

--- src/store/slice/api/auth.ts
+++ src/store/slice/api/auth.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const authApi = createApi({
   reducerPath: 'authApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL, referrerPolicy: 'unsafe-url' }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
   endpoints: (builder) => ({
     login: builder.mutation<AuthType, AuthType>({
       query: (auth) => ({
